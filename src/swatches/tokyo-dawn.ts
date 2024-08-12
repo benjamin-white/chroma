564c745b1cd92c0ca2705d1dc109ef0acb252252
Ben White <benjaminwhite@live.co.uk>
2024-08-13 00:07:03 +0100

    feat: update Tokyo Dawn swatch

--- src/swatches/tokyo-dawn.ts
+++ src/swatches/tokyo-dawn.ts
@@ -1,6 +1,8 @@
 export default {
+  oxfordBlue: "rgb(60, 65, 93)",
   bitter: "rgb(125, 126, 124)",
   seaMoss: "rgb(145, 151, 149)",
   satinLinen: "rgb(234, 231, 220)",
   indianKhaki: "rgb(199, 180, 143)",
+  chestnut: "rgb(184, 91, 76)",
 };
